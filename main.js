4cde80c0601d2a3cbd5941ddb1e46788bce16e2e
Unlatin <chocoaventurero@gmail.com>
2022-01-28 19:30:02 -0500

    first commit

--- main.js
+++ main.js
@@ -1,43 +1,17 @@
-const input = document.querySelector("input");
-const addBtn = document.querySelector(".btn-add");
-const ul = document.querySelector("ul");
-const empty = document.querySelector(".empty");
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
 
-addBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  const text = input.value;
-
-  if (text !== "") {
-    const li = document.createElement("li");
-    const p = document.createElement("p");
-    p.textContent = text;
-
-    li.appendChild(p);
-    li.appendChild(addDeleteBtn());
-    ul.appendChild(li);
-
-    input.value = "";
-    empty.style.display = "none";
-  }
-});
-
-function addDeleteBtn() {
-  const deleteBtn = document.createElement("button");
-
-  deleteBtn.textContent = "X";
-  deleteBtn.className = "btn-delete";
-
-  deleteBtn.addEventListener("click", (e) => {
-    const item = e.target.parentElement;
-    ul.removeChild(item);
-
-    const items = document.querySelectorAll("li");
-
-    if (items.length === 0) {
-      empty.style.display = "block";
-    }
-  });
-
-  return deleteBtn;
-}
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
