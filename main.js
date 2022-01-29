bef9710184dafeb0939f61fa5737a94a2e1f8a99
Unlatin <chocoaventurero@gmail.com>
2022-01-28 22:45:26 -0500

    changes

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
 addToDoButton.addEventListener('click', function(){
+  if ( inputField.value != "") {
+      alert('Your task has been added')
+  } else {
+      alert('You have to write a task')
+  }
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
@@ -14,4 +19,5 @@ addToDoButton.addEventListener('click', function(){
     paragraph.addEventListener('dblclick', function(){
         toDoContainer.removeChild(paragraph);
     })
+    
 })
